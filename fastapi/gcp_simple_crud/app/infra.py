2826e527aba1a0fb25eddc04bf858965e15f45ab
CalebTVanDyke <ctvandyke24@gmail.com>
2024-03-12 17:18:08 -0500

    updates

---
+++ fastapi/gcp_simple_crud/app/infra.py
@@ -0,0 +1,14 @@
+import launchflow as lf
+
+
+# Docs: https://docs.launchflow.com/reference/gcp-resources/cloud-sql
+pg = lf.gcp.CloudSQLPostgres("caleb-postgres3")
+
+# Docs: https://docs.launchflow.com/reference/gcp-resources/gcs-bucket
+gcs_bucket = lf.gcp.GCSBucket("caleb-bucket-unique-bucket-asdf")
+
+# Docs: https://docs.launchflow.com/reference/gcp-resources/compute-engine
+redis_vm = lf.gcp.ComputeEngineRedis("caleb-redis-vm")
+
+# Docs: https://docs.launchflow.com/reference/gcp-resources/memorystore
+redis = lf.gcp.MemorystoreRedis("caleb-redis3")
--- fastapi/gcp_simple_crud/infra.py
+++
@@ -1,5 +0,0 @@
-import launchflow as lf
-
-# TODO(developer): Set these variables with your own values
-db = lf.gcp.CloudSQLPostgres("launchflow-demo-db")
-lf_bucket = lf.gcp.GCSBucket("launchflow-demo-bucket-1")
