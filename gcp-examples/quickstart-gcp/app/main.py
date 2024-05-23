894c47e8ae342ffda32234943a15e170a8c4eabe
tanke <jtanke@uoregon.edu>
2024-05-23 10:07:41 -0700

    temp

--- gcp-examples/quickstart-gcp/app/main.py
+++ gcp-examples/quickstart-gcp/app/main.py
@@ -1,26 +1,8 @@
-from contextlib import asynccontextmanager
-import io
-
-from app.infra import bucket
-
 from fastapi import FastAPI
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await bucket.connect_async()
-    yield
+app = FastAPI()
 
 
-app = FastAPI(lifespan=lifespan)
-
-
-@app.post("/{file_name}")
-async def write_file(file_name: str, file_contents: str):
-    bucket.upload_from_string(file_contents, file_name)
-    return "OK"
-
-
-@app.get("/{file_name}")
-async def read_file(file_name: str) -> str:
-    return bucket.download_file(file_name).decode("utf-8")
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
