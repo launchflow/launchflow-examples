d2c93ac25478b5843fe179f1f925d6aade7157f3
Caleb Van Dyke <caleb@launchflow.com>
2024-06-19 21:43:58 -0500

    chore: testing

--- gcp-examples/quickstart-gcp/app/infra.py
+++ gcp-examples/quickstart-gcp/app/infra.py
@@ -4,11 +4,4 @@ import launchflow as lf
 bucket = lf.gcp.GCSBucket(
     f"launchflow-bucket-for-quickstart-{lf.environment}", force_destroy=True
 )
-ar = lf.gcp.ArtifactRegistryRepository("launchflow-quickstart", "DOCKER")
-service_container = lf.gcp.cloud_run_container.CloudRunServiceContainer(
-    name="my-service-container"
-)
-custom_domain_mapping = lf.gcp.custom_domain_mapping.CustomDomainMapping(
-    "domain-mapping", domain="caleb.test.launchflow.com", cloud_run=service_container
-)
-# service = lf.gcp.CloudRun("launchflow-quickstart", publicly_accessible=False)
+service = lf.gcp.CloudRun("launchflow-quickstart", publicly_accessible=False)
