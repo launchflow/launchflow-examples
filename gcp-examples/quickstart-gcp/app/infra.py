9ddf662430ef40da5e1afb7730916ad08245db50
Caleb Van Dyke <caleb@launchflow.com>
2024-07-19 15:07:10 -0500

    prod testing

--- gcp-examples/quickstart-gcp/app/infra.py
+++ gcp-examples/quickstart-gcp/app/infra.py
@@ -2,6 +2,6 @@ import launchflow as lf
 
 # Docs: https://docs.launchflow.com/reference/gcp-resources/gcs-bucket
 bucket = lf.gcp.GCSBucket(
-    f"launchflow-bucket-for-quickstart-{lf.environment}", force_destroy=True
+    f"launchflow-bucket-for-quickstart-{lf.environment}2", force_destroy=True
 )
 service = lf.gcp.CloudRun("launchflow-quickstart", publicly_accessible=False)
